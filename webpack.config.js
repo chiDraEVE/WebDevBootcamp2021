bbf854d91162883ca775b44c23c73c57dc08bb3d
chidraeve <lukas.mainka@clubitsolutions.de>
2021-05-19 15:14:43 +0200

    webpack für's erste verworfen. Ein Bundle-Tool macht in der Konfiguration nicht wirklich Sinn für viele statische Webseiten

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,16 @@
-const currentTask = process.env.npm_lifecycle_event
-const path = require("path")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
-const WebpackManifestPlugin = require("webpack-manifest-plugin")
+const path = require('path')
 
-const config = {
+module.exports = {
 	entry: "./index.js",
 	output: {
-		filename: "main.[hash].js",
-		path: path.resolve(__dirname, "dist")
+		filename: 'main.js',
+		path: path.resolve(__dirname)
 	},
-	plugins: [new HtmlWebpackPlugin({ template: "./app/index.html" })],
-	mode: "development",
-	devtool: "eval-cheap-source-map",
 	devServer: {
 		before: function(app, server) {
 			server._watch(["./**/*.html"])
 		},
-		port: 3000,
-		contentBase: path.resolve(__dirname),
+		contentBase: path.join(__dirname),
 		hot: true,
 		public: "http://localhost:3000",
 		publicPath: "http://localhost:3000/",
@@ -27,31 +18,5 @@ const config = {
 		headers: {
 			"Access-Control-Allow-Origin": "*"
 		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(sc|sa|c)ss$/,
-				use: ["style-loader", "css-loader", "sass-loader"]
-			},
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: [["@babel/preset-env", { "useBuiltIns": "usage", "corejs": 3, "targets": "defaults" }], "@babel/preset-react"]
-					}
-				}
-			}
-		]
 	}
 }
-
-if (currentTask == "build") {
-	config.mode = "production"
-	config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
-	config.plugins.push(new MiniCssExtractPlugin({ filename: "main.[hash].css" }), new CleanWebpackPlugin(), new WebpackManifestPlugin())
-}
-
-module.exports = config
