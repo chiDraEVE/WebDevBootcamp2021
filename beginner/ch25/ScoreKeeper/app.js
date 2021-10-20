e8a96f109327fd16cc1e823dd5918e624fee07cc
chidraeve <lukas.mainka@clubitsolutions.de>
2021-10-20 22:54:12 +0200

    added a towel break and expanded it with the amount of matches, but with no function yet. Also changed the selection to best of games instead of that strangely feeling winning score

--- beginner/ch25/ScoreKeeper/app.js
+++ beginner/ch25/ScoreKeeper/app.js
@@ -1,31 +1,53 @@
 const p1 = {
 	score: 0,
+	games: 0,
 	button: document.querySelector('#p1Button'),
-	display: document.querySelector('#p1Display')
+	display: document.querySelector('#p1Display'),
+	gamesDisplay: document.querySelector('#p1MatchesDisplay')
 }
 const p2 = {
 	score: 0,
+	games: 0,
 	button: document.querySelector('#p2Button'),
-	display: document.querySelector('#p2Display')
+	display: document.querySelector('#p2Display'),
+	gamesDisplay: document.querySelector('#p2MatchesDisplay')
 }
 
+const message = document.querySelector('#message')
 const resetButton = document.querySelector('#reset')
-const winningScoreSelect = document.querySelector('#playto')
-let winningScore = 5
-let isGameOver = false
+const bestOfGamesSelect = document.querySelector('#bestOfGames')
+const winningScore = 11
+let maxMatches = 5
+
+let isGameOver = (score1, score2) => {
+	if (score1 < winningScore && score2 < winningScore)
+		return false
+	else return Math.abs(score1 - score2) >= 2;
+}
 
 function updateScores(player, opponent) {
-	if (!isGameOver) {
+	if (!isGameOver(player.score, opponent.score)) {
 		player.score += 1
-		if (player.score === winningScore) {
-			isGameOver = true
+		if (isGameOver(player.score, opponent.score)) {
 			player.display.classList.add('has-text-success')
 			opponent.display.classList.add('has-text-danger')
 			player.button.disabled = true
 			opponent.button.disabled = true
 		}
 		player.display.textContent = player.score
+		displayTowelBreak(p1.score, p2.score)
 	}
+}
+
+function displayTowelBreak(score1, score2) {
+	const isTowelBreak = (sum) => {
+		return sum % 6 === 0;
+	}
+
+	if (isTowelBreak(score1 + score2))
+		message.textContent = "Towel Break"
+	else
+		message.textContent = ""
 }
 
 p1.button.addEventListener('click', function() {
@@ -36,15 +58,14 @@ p2.button.addEventListener('click', function() {
 	updateScores(p2, p1)
 })
 
-winningScoreSelect.addEventListener("change", function () {
-	winningScore = parseInt(this.value)
-	reset()
+bestOfGamesSelect.addEventListener("change", function () {
+	maxMatches = parseInt(this.value)
+	console.log(maxMatches)
 })
 
 resetButton.addEventListener("click", reset)
 
 function reset() {
-	isGameOver = false
 	for (let p of [p1, p2]) {
 		p.score = 0
 		p.display.textContent = p.score
