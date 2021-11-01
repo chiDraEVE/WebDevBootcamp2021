82ad0857e16ab2ef29839da1db2009929eb1d814
chidraeve <lukas.mainka@clubitsolutions.de>
2021-11-01 17:09:39 +0100

    finished Ch 32: Exploring Modules & The NPM Universe

--- beginner/ch32/LangGuessr/index.js
+++ beginner/ch32/LangGuessr/index.js
@@ -1,3 +1,12 @@
 // REMEMBER TO RUN "npm install" in your terminal (in the same directory as this file)
-
-
+const franc = require('franc')
+const langs = require('langs')
+const colors = require('colors')
+const input = process.argv[2]
+const langCode = franc(input)
+if (langCode === "und") {
+	console.log("Sorry, couldn't figure it out! Try with more sample text!".red)
+} else {
+	const language = langs.where("3", langCode)
+	console.log(`Our best guess is ${language.name}`.green)
+}
